97fbbd89e61afeb4c43fb3e974b2c6f62f61b48a
Knife Club <hasankilic@hotmail.com>
2024-05-09 00:43:06 +0300

    commited

---
+++ app/src/main/java/com/hasankilic/kotlinmaps/MapsActivity.kt
@@ -0,0 +1,45 @@
+package com.hasankilic.kotlinmaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.hasankilic.kotlinmaps.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+        //latitude , longitude
+
+        //lat-> 48.85391 , lon->2.2913515
+
+
+        val eiffel = LatLng(48.85391,2.2913515)
+        mMap.addMarker(MarkerOptions().position(eiffel).title("Eiffel Tower"))//ikon
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eiffel,15f))//zoomlama
+    }
+}
